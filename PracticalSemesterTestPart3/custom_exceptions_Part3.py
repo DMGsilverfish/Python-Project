dde354f1916db4e3ff55999bc8897484a378a4a0
Darkk7 <mvlok2001@gmail.com>
2023-09-26 10:38:07 +0200

    removed unnecessary files
    From Matt 26 September 2023

--- PracticalSemesterTestPart3/custom_exceptions_Part3.py
+++
@@ -1,5 +0,0 @@
-# custom_exceptions.py
-
-class FileImportError(OSError):
-    def __init__(self, message):
-        super().__init__(message)
--- PracticalSemesterTestPart3/region_Part3.py
+++
@@ -1,5 +0,0 @@
-class Region:
-    def __init__(self, code, name):
-        self.code = code
-        self.name = name
-        
